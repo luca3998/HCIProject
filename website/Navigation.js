1bcf986a65f788de096f8deb030629321d01b6ef
Luca <luca@mbp-van-luca.home>
2021-12-06 01:09:56 +0100

    Adds svg transformations and rotations based on direction

--- website/Navigation.js
+++ website/Navigation.js
@@ -137,3 +137,76 @@ function toggleZoom(){
     }
 }
 
+let steps = [];
+let currentStep = 0;
+const path = shortestPath(localStorage.getItem('start'),localStorage.getItem('end'));
+
+function generateSteps(){
+    const amountOfSteps = path.length - 1;
+    console.log(amountOfSteps);
+    for(let j = 0; j <= amountOfSteps;j++){
+        steps.push({
+            step: j, description: "test"
+        });
+    }
+    document.getElementById("description_container").innerHTML = steps[currentStep].description;
+    document.getElementById("step_number").innerHTML = "Step " + (currentStep+1);
+    updateMap();
+}
+
+function previousStep(){
+    //  update header
+    // update descriptions
+    // Update Map
+    currentStep -= 1;
+    document.getElementById("step_number").innerHTML = "Step " + currentStep;
+    document.getElementById("description_container").innerHTML = steps[currentStep].description;
+    updateMap();
+    
+}
+
+function nextStep(){
+    //  update header
+    // update descriptions
+    // Update Map
+    currentStep += 1;
+    document.getElementById("step_number").innerHTML = "Step " + currentStep;
+    document.getElementById("description_container").innerHTML = steps[currentStep].description;
+    updateMap()
+}
+
+function updateMap(){
+    // Calculate x1, x2, change between both, make sure way up is the right one 
+    let svg = document.querySelector("svg");
+    let x1 = graph.vertices[path[currentStep]].x; 
+    let y1 = graph.vertices[path[currentStep]].y; 
+    let x2 = graph.vertices[path[currentStep+1]].x; 
+    let y2 = graph.vertices[path[currentStep+1]].y; 
+    let middle_x = (x1 + x2) / 2;
+    let middle_y = (y1 + y2) / 2;
+    svg.setAttribute("viewBox", (middle_x-150) + ' ' + (middle_y-150) + " 300 300"); 
+    let diff_x = x1 - x2; // if positive, west, negative, east
+    let diff_y = y1 - y2; // if positive, north, negative, south
+
+    let direction = '';
+    let rotation = 0;
+
+        if(diff_x > 0 && Math.abs(diff_x) > Math.abs(diff_y)){
+            direction = 'W';
+            rotation = 90;
+        } else if(diff_x < 0 && Math.abs(diff_x) > Math.abs(diff_y)){
+            direction = 'E';
+            rotation = -90;
+        }
+        if(diff_y > 0 && Math.abs(diff_y) > Math.abs(diff_x)){
+            direction = 'N';
+            rotation = 0;
+        } else if(diff_y < 0 && Math.abs(diff_y) > Math.abs(diff_x)){
+            direction = 'S';
+            rotation = 180;
+        }
+    
+    svg.setAttribute("transform","rotate("+rotation+")");
+
+    // make sure up is the right one.
+}
